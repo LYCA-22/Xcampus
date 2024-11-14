c7a45afb34e03e92ccdd517b7887b83412c787b3
Zhicheng Gong <109425to@gmail.com>
2024-11-15 01:02:07 +0800

    fixed icons

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function Mobliebar(){
           </svg>
         </Link>
         <button className='sparkbtn'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23ç" viewBox="0 0 24 24" fill="none"
+          <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none"
                stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                className="icon icon-tabler icons-tabler-outline icon-tabler-flare">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
