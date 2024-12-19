dc33e7da0c152e31c425430fdda9ba3440a9a5e1
Zhicheng Gong <109425to@gmail.com>
2024-12-19 22:46:16 +0800

    test back

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Index() {
     const { user, userName } = useAuth()
 
     return (
-        <section>
+        <section className='home'>
             {userName ? <h1 className="sitetitle" >歡迎, {userName}</h1> : <h1 className="sitetitle" >載入資料中</h1>}
         </section>
     );
