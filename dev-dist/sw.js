68436cc2bacf49e1b2ae4b2f61fcfda5621fb86e
Zhicheng Gong <109425to@gmail.com>
2024-12-20 20:30:11 +0800

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7krfeodoe0o"
+    "revision": "0.nvhopefp7jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.jsx
+++ src/App.jsx
@@ -193,17 +193,6 @@ function App() {
 
   useEffect(() => {
     CheckUser();
-
-    const updateSW = registerSW({
-      onNeedRefresh() {
-        // 當有新版本時的處理
-        updateSW()
-      },
-      onOfflineReady() {
-        // 離線功能準備就緒時的處理
-        console.log('App ready to work offline')
-      },
-    })
   }, []);
 
   const Footer = () => {
